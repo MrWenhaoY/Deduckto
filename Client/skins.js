6d84bc56889614e6e16427ed4f52010a15ef1672
Wenhao <wenhaoyang2015@gmail.com>
2025-10-20 23:58:34 -0400

    Two text skins

---
+++ Client/skins.js
@@ -0,0 +1,23 @@
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+const GREEK = "αβγδεζηθικλμνξοπρσςτυφχψω";
+const COMMONGREEK = "αβεθλμπδτηω";
+const digits = "0123456789";
+
+const SKINS = {
+    tuple: {
+        name: "tuple",
+        type: "text",
+        parse: (card) => "(" + card + ")",
+        cat0_names: digits,
+        cat1_names: digits,
+        cat2_names: digits
+    },
+    greek: {
+        name: "greek",
+        type: "text",
+        parse: (card) => "[" + ALPHABET[card[0]] + card[1] + COMMONGREEK[card[2]] + "]",
+        cat0_names: ALPHABET,
+        cat1_names: digits,
+        cat2_names: COMMONGREEK
+    }
+}
