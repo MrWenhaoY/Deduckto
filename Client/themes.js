d1b07b93947c61fd2536ab29d083dcad52987138
Wenhao <wenhaoyang2015@gmail.com>
2026-02-07 01:13:24 -0500

    Remove text themes

--- Client/themes.js
+++ Client/themes.js
@@ -4,26 +4,26 @@ const COMMONGREEK = "αβεθλμπδτηω";
 const digits = "0123456789";
 
 const THEMES = {
-    tuple: {
-        name: "tuple",
-        type: "text",
-        max_size: 10,
-        parse: (card) => "(" + card + ")",
-        null: () => "(???)",
-        cat0_names: digits,
-        cat1_names: digits,
-        cat2_names: digits
-    },
-    greek: {
-        name: "greek",
-        type: "text",
-        max_size: 10,
-        parse: (card) => "[" + ALPHABET[card[0]] + card[1] + COMMONGREEK[card[2]] + "]",
-        null: () => "[???]",
-        cat0_names: ALPHABET,
-        cat1_names: digits,
-        cat2_names: COMMONGREEK
-    },
+    // tuple: {
+    //     name: "tuple",
+    //     type: "text",
+    //     max_size: 10,
+    //     parse: (card) => "(" + card + ")",
+    //     null: () => "(???)",
+    //     cat0_names: digits,
+    //     cat1_names: digits,
+    //     cat2_names: digits
+    // },
+    // greek: {
+    //     name: "greek",
+    //     type: "text",
+    //     max_size: 10,
+    //     parse: (card) => "[" + ALPHABET[card[0]] + card[1] + COMMONGREEK[card[2]] + "]",
+    //     null: () => "[???]",
+    //     cat0_names: ALPHABET,
+    //     cat1_names: digits,
+    //     cat2_names: COMMONGREEK
+    // },
     maple: {
         name: "maple",
         type: "image",
@@ -31,6 +31,21 @@ const THEMES = {
         cat0_root: "color/",
         cat0_parse: (i) => i >= 0 ? "color" + i + ".png" : "color_unknown.png",
         cat0_names: ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink"],
+        cat1_root: "person/",
+        cat1_parse: (i) => i >= 0 ? "person" + i + ".png" : "person_unknown.png",
+        cat1_names: ["Seven", "Arks", "Winter", "Gart", "Eli", "Lain", "Del Fuego"],
+        cat2_root: "item/",
+        cat2_parse: (i) => i >= 0 ? "item" + i + ".png" : "blank.png",
+        cat2_names: ["Water Bottle", "iPad", "Rocket", "Clarinet", "Flask", "Cookie", "Chessboard"],
+        toText(card) {return this.cat0_names[card[0]] + " " + this.cat1_names[card[1]] + " with " + this.cat2_names[card[2]]}
+    },
+    animals: {
+        name: "animals",
+        type: "image",
+        max_size: 7,
+        cat0_root: "locations/",
+        cat0_parse: (i) => i >= 0 ? "location" + i + ".png" : "location_unknown.png",
+        cat0_names: ["Mountains", "Library"],
         cat1_root: "person/",
         cat1_parse: (i) => i >= 0 ? "person" + i + ".png" : "person_unknown.png",
         cat1_names: ["Seven", "Arks", "Winter", "Gart", "Eli", "Lain", "Del Fuego"],
